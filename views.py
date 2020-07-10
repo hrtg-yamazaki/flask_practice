d773ca33544f141f0c46366b9e0bb13b14d52017
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-07-10 20:03:50 +0900

    [add] 初期設定
    ・開発サーバーの実行と表示確認
    ・.gitignoreの作成

---
+++ views.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+html = """\
+<h1 style='margin:0; color:navy;'>Hello, world!</h1>
+"""
+
+
+@app.route("/")
+def index():
+    return html
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5020)
